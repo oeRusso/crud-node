1225e1c8357bedc70b6b45848dbd0e2fc111ec5d
Esteban Russo <oerusso28@gmail.com>
2023-08-25 19:08:33 -0300

    CRUD BASICO TERMINADO

--- app.js
+++ app.js
@@ -15,17 +15,34 @@ conexion.connect(err => {
 })
 
 // MOSTRAR
-conexion.query("SELECT * FROM	usuarios", (err, filas) => {
-    if (err) {
-        throw err;
-    }else{
-        filas.forEach( fila => {
-            console.log(filas);
-        })
-    }
+conexion.query('SELECT * FROM	usuarios', (err, filas) => {
+  if (err) {
+    throw err
+  } else {
+    filas.forEach(fila => {
+      console.log(filas)
+    })
+  }
 })
 
 // INSERTAR
 
-conexion.query("INSERT INTO usuarios (nombre,email,contraseña) VALUES ("Normar","Chavez","jeje123")";
-conexion.end();
+/* conexion.query("INSERT INTO usuarios (nombre,email,contraseña) VALUES ('Normar','Chavez','jeje123')",(err,result)=>{
+
+if(err) throw err;
+console.log('Registro agregado',result);
+}); */
+
+// EDITAR
+// conexion.query("UPDATE  usuarios SET nombre='cesar',email='asdasdaqgamil.com', contraseña='barnis' WHERE id=5",(err,result)=>{
+//   if(err) throw err;
+//   console.log('Registro actualizado',result);
+// });
+
+// ELIMINAR
+conexion.query('DELETE FROM usuarios WHERE id=1', (err, result) => {
+  if (err) throw err
+  console.log('Registro eliminado', result)
+})
+
+conexion.end()
